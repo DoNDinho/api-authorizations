636aa3fec013fe327c5526b20c4e8a474f21ec66
Diego Flores <ext_dfloresr@falabella.cl>
2021-09-17 23:24:23 -0300

    Prueba 04

--- src/data/repository/login.repository.js
+++ src/data/repository/login.repository.js
@@ -5,12 +5,12 @@ const sqlProcedures = require('../database/oracle/sql_procedures')
 const authenticateUser = async (email) => {
 	try {
 		console.log('Entrando a login repository') // TODO borrar esto
-		//const database = new Runner()
+		const database = new Runner()
 		const procedure = sqlProcedures.authenticateUser(email)
 		console.log('El procedimiento es ', JSON.stringify(procedure)) // TODO borrar esto
-		//const result = await database.runProcedure(procedure)
+		const result = await database.runProcedure(procedure)
 		console.log('Saliendo login repository') // TODO borrar esto
-		//return result.outBinds
+		return result.outBinds
 		return {
 			P_CODIGO: '000',
 			P_MENSAJE: 'Exito',
