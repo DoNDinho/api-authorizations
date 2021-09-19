48e57a1526157897e781cc2efc69a16584c30771
Diego Flores <ext_dfloresr@falabella.cl>
2021-09-19 01:46:09 -0300

    Se despliega version inicial de API

--- src/data/repository/login.repository.js
+++ src/data/repository/login.repository.js
@@ -4,7 +4,6 @@ const sqlProcedures = require('../database/oracle/sql_procedures')
 
 const authenticateUser = async (email) => {
 	try {
-		logger.info('Entrando a login repository') // TODO borrar esto
 		const database = new Runner()
 		const procedure = sqlProcedures.authenticateUser(email)
 		const result = await database.runProcedure(procedure)
