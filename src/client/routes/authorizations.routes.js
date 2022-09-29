ede0b68b76eb9718cf5475ad16104c0b85ba7667
Diego Flores <ext_dfloresr@Falabella.cl>
2022-09-29 00:25:07 -0300

    deploy

--- src/client/routes/authorizations.routes.js
+++ src/client/routes/authorizations.routes.js
@@ -2,12 +2,12 @@ const express = require('express')
 const validateTokenService = require('../../business/services/validate-token')
 const router = express.Router()
 
-router.post(`/Authorizations/v1/authentications/:token/validate`, async (req, res, next) => {
+router.get(`/Authorizations/v1/authentications/:token/validate`, async (req, res, next) => {
 	try {
 		await validateTokenService.execute(req.params.token)
 		const message = 'Autorizacion exitosa'
-		logger.info({ message, data: JSON.stringify(response) })
-		res.json(message)
+		logger.info({ message })
+		res.json({ message })
 	} catch (error) {
 		console.log('error: ', error.message)
 		next(error)
--- src/data/database/my-sql/runner.js
+++ src/data/database/my-sql/runner.js
@@ -1,4 +1,6 @@
 'use strict'
+const mysql = require('mysql2/promise')
+
 class MySqlRunner {
 	constructor(client) {
 		this.connection = client.getConnection()
@@ -7,15 +9,33 @@ class MySqlRunner {
 	async runProcedure(procedure) {
 		try {
 			const result = []
+			// await this.connect()
 			for (const statement of procedure.statements) {
 				const [rows] = await this.connection.execute(statement, procedure.values)
 				result.push(rows)
 			}
+			// this.close()
 			return result
 		} catch (error) {
 			console.log(`error al ejecutar procedimiento ${procedure.name}. ${error.message}`)
 			throw error
 		}
+	}
+
+	async connect() {
+		const connection = await mysql.createConnection({
+			host: process.env.MYSQL_HOST,
+			user: process.env.MYSQL_USER,
+			password: process.env.MYSQL_PASS,
+			database: process.env.MYSQL_DATABASE,
+			multipleStatements: true
+		})
+
+		this.connection = connection
+	}
+
+	async close() {
+		await this.connection.end()
 	}
 }
 
