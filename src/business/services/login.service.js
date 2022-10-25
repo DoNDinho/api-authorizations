b673b4499976663f89f7f0d59bf390ebb7993c0e
Flores Roman <diego.a.flores.roman@accenture.com>
2022-10-25 00:53:48 -0300

    deploy

--- src/business/services/login.service.js
+++ src/business/services/login.service.js
@@ -5,43 +5,43 @@ const loginRepository = require('../../data/repository/login.repository')
 const loginConverter = require('../converter/login.converter')
 
 const login = async (data) => {
-	try {
-		const userData = await authenticateUser(data.username)
-		validatePassword(data.password, userData['@PASSWORD'])
-		const tokenData = generateToken(data.username, userData)
-		return loginConverter.parseLoginResponse(tokenData, userData)
-	} catch (error) {
-		throw error
-	}
+  try {
+    const userData = await authenticateUser(data.username)
+    validatePassword(data.password, userData['@PASSWORD'])
+    const tokenData = generateToken(data.username, userData)
+    return loginConverter.parseLoginResponse(tokenData, userData)
+  } catch (error) {
+    throw error
+  }
 }
 
 const authenticateUser = async (email) => {
-	try {
-		const result = await loginRepository.authenticateUser(email)
-		return result
-	} catch (error) {
-		throw { httpCode: 401, message: error.message }
-	}
+  try {
+    const result = await loginRepository.authenticateUser(email)
+    return result
+  } catch (error) {
+    throw { httpCode: 401, message: error.message }
+  }
 }
 
 const validatePassword = (passwordRQ, passwordBD) => {
-	const validate = bcrypt.compareSync(passwordRQ, passwordBD)
-	if (!validate) {
-		logger.error('Password invalido')
-		throw { httpCode: 401, message: 'Credenciales invalidas' }
-	}
+  const validate = bcrypt.compareSync(passwordRQ, passwordBD)
+  if (!validate) {
+    logger.error('Password invalido')
+    throw { httpCode: 401, message: 'Credenciales inválidas' }
+  }
 }
 
 const generateToken = (email, userData) => {
-	const payload = new Object()
-	payload.username = email
-	payload.rolId = userData['@ROL']
-	payload.rolRescription = userData['@DESCRIPTION']
+  const payload = new Object()
+  payload.username = email
+  payload.rolId = userData['@ROL']
+  payload.rolRescription = userData['@DESCRIPTION']
 
-	const token = jwt.sign(payload, process.env.JWT_SIGNATURE, {
-		expiresIn: process.env.JWT_EXPIRATION
-	})
-	return { token, expiresIn: '36000' }
+  const token = jwt.sign(payload, process.env.JWT_SIGNATURE, {
+    expiresIn: process.env.JWT_EXPIRATION
+  })
+  return { token, expiresIn: '36000' }
 }
 
 module.exports = { login }
